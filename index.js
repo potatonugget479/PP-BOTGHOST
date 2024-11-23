6996d554fbb9a9c44d3d9e096f8660248056ab7f
potatonugget479 <potatonugget479@gmail.com>
2024-11-24 04:25:57 +1300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Managing Platinum Productions", "🎮 Playing VALORANT"];
+const statusMessages = ["Managing Platinum Productions"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
